f295cd1b88bae4f4b217b5d61c585a1dde8e7869
abshriva <shaky2108@gmail.com>
2017-03-14 12:30:12 +0530

    add
    add

---
+++ Scripts/app1.js
@@ -0,0 +1,13 @@
+function getEmployeedetailsbyVar()
+{
+     var secondDate =  new Date(1993,07,21); 
+     var name ="Abhishek";
+     var Age =24;
+     var DateOfBirth=secondDate;
+     var PlaceOfBirth = "Gwalior";
+
+     console.log("Name: "+name);
+     console.log("Age: "+Age);
+     console.log("DOB: "+DateOfBirth);
+     console.log("PlaceOfBirth: "+PlaceOfBirth);
+}
